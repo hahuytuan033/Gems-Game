751245c47380f6fbfd0ff5da69e562afb6e56fb5
huytuan033 <hahuytuan033@gmail.com>
2024-10-03 03:28:33 +0700

    unlocked Level

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -6,51 +6,41 @@ using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public Button[] levelButton;
-    public Image Lock;
-    public Image Done;
-    private int highestLevel;
-
     public GameObject panel;
-    public GameObject menuPanel;
+    public GameObject menu;
+    public Button playButton;
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        //unlock level
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        highestLevel = PlayerPrefs.GetInt("highestLevel", 1);
-
-        for (int i = 0; i < levelButton.Length; i++)
-        {
-            int levelNum = i + 1;
-            if (levelNum > highestLevel)
-            {
-                levelButton[i].interactable = false;
-                levelButton[i].GetComponent<Image>().sprite = Lock.sprite;
-                levelButton[i].GetComponentInChildren<Text>().text = "";
-            }
-            else
-            {
-                levelButton[i].interactable = true;
-                levelButton[i].GetComponentInChildren<Text>().text= ""+ levelNum;
-                levelButton[i].GetComponent<Image>().sprite= Done.sprite;
-            }
-        }
+        playButton.onClick.AddListener(MenuPanel);
     }
 
-    public void LoadLevel(int levelNum)
-    {
-        SceneManager.LoadScene("Leve_"+ levelNum);
-    }
-
-    public void Reset()
-    {
-        PlayerPrefs.DeleteAll();
-        PlayerPrefs.Save();
-    }
-
-    public void SwitchPanel()
+    public void MenuPanel()
     {
         panel.SetActive(false);
-        menuPanel.SetActive(true);
+        menu.SetActive(true);
     }
+
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "Level " + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+
 }
