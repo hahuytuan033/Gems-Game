41b07b0f398798e0e23d186fa42e78d299690603
huytuan033 <hahuytuan033@gmail.com>
2024-10-08 23:31:19 +0700

    Moving the pieces

--- Assets/Script/Dot.cs
+++ Assets/Script/Dot.cs
@@ -4,21 +4,63 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    public int col;
+    public int row;
+    private Board board;
+    private GameObject otherDot;
+    public int targetX;
+    public int targetY;
+
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
     public float swipeAngle = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        col = targetX;
     }
 
     // Update is called once per frame
     void Update()
     {
+        targetX = col;
+        targetY = row;
+        // move to horizontal
+        if (Mathf.Abs(targetX - transform.position.x) > 1)
+        {
+            // move towards the target
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            //directly set the position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allDots[col, row]= this.gameObject;
+        }
 
+        // move to vertical
+        if (Mathf.Abs(targetY - transform.position.y) > 1)
+        {
+            // move towards the target
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            //directly set the position
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allDots[col, row]= this.gameObject;
+        }
     }
 
     private void OnMouseDown()
@@ -36,6 +78,39 @@ public class Dot : MonoBehaviour
     void CalculateAngle()
     {
         swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log(swipeAngle);
+        // Debug.Log(swipeAngle);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && col < board.width)
+        {
+            // right swipe
+            otherDot = board.allDots[col + 1, row];
+            otherDot.GetComponent<Dot>().col -= 1;
+            col += 1;
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)
+        {
+            // up swipe
+            otherDot = board.allDots[col, row + 1];
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && col > 0)
+        {
+            // left swipe
+            otherDot = board.allDots[col - 1, row];
+            otherDot.GetComponent<Dot>().col += 1;
+            col -= 1;
+        }
+        else if (swipeAngle > -45 && swipeAngle >= -135 && row > 0)
+        {
+            // down swipe
+            otherDot = board.allDots[col, row - 1];
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+        }
     }
 }
